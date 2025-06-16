ed86f6d0d3074c84cbe9f12b57b0e3d050c4f198
Xeras <chrisn5413@gmail.com>
2025-06-16 03:32:50 -0700

    starting derivatives chapter

--- NNFromScratchBook.py
+++ NNFromScratchBook.py
@@ -100,11 +100,68 @@ plt.show()'''
 # contains multiple stages of making a neural network from complex to simple
 # Just choose one to run
 def main():
-    two_dense_two_active_loss()
+    neural_network()
+    # two_dense_two_active_loss_accuracy()
     # two_dense_two_active()
     # one_dense_one_active()
     # one_dense()
     # one_dense_classless()
+
+
+def neural_network():
+    # Create model
+    dense1 = Layer_Dense(2, 3)  # first dense layer, 2 inputs
+    activation1 = Activation_ReLU()
+    dense2 = Layer_Dense(3, 3)  # second dense layer, 3 inputs, 3 outputs
+    activation2 = Activation_Softmax()
+
+    # Create loss function
+    loss_function = Loss_CategoricalCrossEntropy()
+    # Helper variables
+    lowest_loss = 9999999  # some initial value
+    best_dense1_weights = dense1.weights.copy()
+    best_dense1_biases = dense1.biases.copy()
+    best_dense2_weights = dense2.weights.copy()
+    best_dense2_biases = dense2.biases.copy()
+
+    for iteration in range(10000):
+        # Update weights with some small random values
+        dense1.weights += 0.05 * np.random.randn(2, 3)
+        dense1.biases += 0.05 * np.random.randn(1, 3)
+        dense2.weights += 0.05 * np.random.randn(3, 3)
+        dense2.biases += 0.05 * np.random.randn(1, 3)
+
+        # Perform a forward pass of our training data through this layer
+        dense1.forward(X)
+        activation1.forward(dense1.output)
+        dense2.forward(activation1.output)
+        activation2.forward(dense2.output)
+
+        # Perform a forward pass through activation function
+        # it takes the output of second dense layer here and returns loss
+        loss = loss_function.calculate(activation2.output, y)
+
+        # Calculate accuracy from output of activation2 and targets
+        # calculate values along first axis
+        predictions = np.argmax(activation2.output, axis=1)
+        accuracy = np.mean(predictions == y)
+
+        # If loss is smaller - print and save weights and biases aside
+        if loss < lowest_loss:
+            print('New set of weights found, iteration:', iteration,
+                  'loss:', loss, 'acc:', accuracy)
+            best_dense1_weights = dense1.weights.copy()
+            best_dense1_biases = dense1.biases.copy()
+            best_dense2_weights = dense2.weights.copy()
+            best_dense2_biases = dense2.biases.copy()
+            lowest_loss = loss
+
+        # Revert weights and biases
+        else:
+            dense1.weights = best_dense1_weights.copy()
+            dense1.biases = best_dense1_biases.copy()
+            dense2.weights = best_dense2_weights.copy()
+            dense2.biases = best_dense2_biases.copy()
 
 
 # Two layer dense
@@ -112,7 +169,7 @@ def main():
 # Softmax activation for Dense2
 # Loss (error) calculation
 # Accuracy (correct guesses) calculation
-def two_dense_two_active_loss():
+def two_dense_two_active_loss_accuracy():
     # Create Dense layer with 2 input features and 3 output values
     # Create ReLU activation (used with Dense Layer)
     dense1 = Layer_Dense(2, 3)
